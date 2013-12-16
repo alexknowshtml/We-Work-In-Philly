423f22a2055958be5f0fb8515a494aeb3ec7a0f7
Keith Fitzgerald <kfitzgerald@gmail.com>
2013-12-16 17:55:04 -0500

    attempt to fix precompilation asset issue

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Citizenry
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths += Dir["."]
+    
+    # fix for 3.x rails apps on heroku https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
